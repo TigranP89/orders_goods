39e1a39d436c5078c4c69ece4e840d066f335d2b
TigranP89 <62433823+TigranP89@users.noreply.github.com>
2023-02-07 23:54:41 +0400

    Create some controllers and migrations

---
+++ app/Http/Controllers/Shop/BaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Shop;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    //
+}
